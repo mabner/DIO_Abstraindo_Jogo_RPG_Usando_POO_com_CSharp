528452f6e527dda4a52d3f337b5e211925c10e21
Marcos Leite <m.abner@live.co.uk>
2022-03-10 02:30:09 -0300

    hero constructor method

--- src/Entities/Marco.cs
+++ src/Entities/Marco.cs
@@ -2,6 +2,13 @@ namespace code.src.Entities
 {
 	public class Marco
 	{
+		public Marco(string Name, int Level, string HeroType)
+		{
+			this.Name = Name;
+			this.Level = Level;
+			this.HeroType = HeroType;
+		}
+
 		public string Name;
 
 		public int Level;
