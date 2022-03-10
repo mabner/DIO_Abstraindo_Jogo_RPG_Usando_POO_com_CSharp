0d42477dd22925c43ba503c7644e884f34374584
Marcos Leite <m.abner@live.co.uk>
2022-03-10 02:02:44 -0300

    representing the characters with structured language paradigm

--- Program.cs
+++ Program.cs
@@ -4,7 +4,30 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			/*
+			Major Marco Rossi
+			Captain Tarma Roving
+			Sergeant Second Class Eri Kasamoto
+			Master Sergeant Fiolina "Fio" Germi
+			Nadia Cassel
+			Trevor Spacey
+			Cadet Walter Ryan
+			Cadet Tyra Elson
+			Colonel Ralf Jones
+			Second Lieutenant Clark Stil
+			Leona Heidern			
+			*/
+			string name = "Marco Rossi";
+			int level = 14;
+			string heroType = "Major";
+
+			string name2 = "Tarma Roving";
+			int level2 = 16;
+			string heroType2 = "Captain";
+
+			System.Console.WriteLine(name + " " + level + " " + heroType);
+			System.Console.WriteLine(name2 + " " + level2 + " " + heroType2);
+
 		}
 	}
 }
